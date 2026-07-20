bd954f8637a89fca14c6c4cbd66957c628ac0367
scripting-alt <hfelipegabrielpc09@gmail.com>
2026-07-20 00:47:53 +0000

    GG

--- push.sh
+++ push.sh
@@ -3,5 +3,10 @@
 read -p "Mensagem do commit: " msg
 
 git add .
-git commit -m "$msg"
-git push
+
+if git diff --cached --quiet; then
+    echo "Nenhuma alteração para commit."
+    exit 0
+fi
+
+git commit -m "$msg" && git push
