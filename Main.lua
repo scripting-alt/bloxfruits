0f83e2514ff875f65deecd2c559b071da391d73b
alguem <hfelipegabrielpc09@gmail.com>
2026-08-11 20:46:25 -0300

    Update11

--- Main.lua
+++ Main.lua
@@ -2,8 +2,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau"))()
 
 ScriptVersion = {
-    Version = "v1.9.5",
-    Date = "2026-08-08"
+    Version = "v1.9.8",
+    Date = "2026-08-11"
 }
 
 _G.SelectTool = "Melee"
