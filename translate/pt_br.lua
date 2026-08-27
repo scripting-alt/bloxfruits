636e763c7c291ecd57dfb10f6477541f95330de8
alguem <hfelipegabrielpc09@gmail.com>
2026-08-26 22:45:16 -0300

    GG

--- translate/pt_br.lua
+++ translate/pt_br.lua
@@ -1,3 +1,11 @@
-{
-    ["Auto Farm Level"] = "Farm Nível Automático"
+return {
+    ["Auto Farm Level"] = "Farm Nível Automático",
+    ["Level Farm"] = "Farm de Nível",
+
+    ["Auto Farm Nearest"] = "Farm Mais Próximo",
+    ["Farm Nearest Mobs"] = "Farm de Mobs Mais Próximos",
+
+    ["Distance Aimbot"] = "Aimbot do Distância",
+    ["Fast Attack"] = "Ataque Rápido",
+    ["Fast Attack Speed"] = "Velocidade do Ataque Rápido",
 }
